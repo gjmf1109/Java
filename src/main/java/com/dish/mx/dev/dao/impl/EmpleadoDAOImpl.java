6c8c090aa457c9b2bad0c16e7c2a7da08980ba7f
Gerardo Martinez <gerardo.martinez@dish.com.mx>
2018-04-17 13:23:33 -0500

    Clase EmpleadoDAOImpl documentada

--- src/main/java/com/dish/mx/dev/dao/impl/EmpleadoDAOImpl.java
+++ src/main/java/com/dish/mx/dev/dao/impl/EmpleadoDAOImpl.java
@@ -10,14 +10,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Clase en la que se implementan la interface EmpleadoDAO con todos los métodos
+ * que ejecutan los querys para el manejo de los datos de la base.
+ *
+ * @version 0.0.1
+ *
+ * @author Gerardo Martinez &lt;gerardo.martinez@dish.com.mx%gt;
+ *
+ * @since 0.0.1
+ *
+ */
 @Repository("empleadoDAOImpl")
-public class EmpleadoDAOImpl implements EmpleadoDAO{
+public class EmpleadoDAOImpl implements EmpleadoDAO {
+
     private static final Logger logger = LogManager.getLogger(EmpleadoDAOImpl.class);
-    
+
     @Autowired
     private JdbcTemplate jdbcTemplate;
-    
-    //Constructor vacío
+
+    /**
+     * Constructor vacío de la clase.
+     *
+     * @author Gerardo Martinez &lt;gerardo.martinez@dish.com.mx&gt;
+     *
+     * @since 0.0.1
+     */
     public EmpleadoDAOImpl() {
     }
 
@@ -25,14 +43,34 @@ public class EmpleadoDAOImpl implements EmpleadoDAO{
 //    public EmpleadoDAOImpl(JdbcTemplate jdbcTemplate) {
 //        this.jdbcTemplate = jdbcTemplate;
 //    }
-
-    //Leer a la base de datos
+    
+    /**
+     * El método ejecuta un query para poder obtener todos los registros de la
+     * base de datos
+     *
+     * @return Regresa una lista con los registros obtenidos por el query
+     *
+     * @author Gerardo Martinez &lt;gerardo.martinez@dish.com.mx&gt;
+     *
+     * @since 0.0.1
+     */
     @Override
     public List<EmpleadoDTO> encontrarTodos() {
         return getJdbcTemplate().query("SELECT * FROM empleado", new EmpleadoRowMapper());
     }
 
-    //Leer base ingresando por ID
+    /**
+     * El método ejecuta un query para poder obtener todos los registros de la
+     * base de datos según el id introducido
+     *
+     * @param id
+     * @return Regresa un objeto del tipo Empleado con los registros obtenidos
+     * por el query
+     *
+     * @author Gerardo Martinez &lt;gerardo.martinez@dish.com.mx&gt;
+     *
+     * @since 0.0.1
+     */
     @Override
     public EmpleadoDTO encontrarPorId(int id) {
         //Ejecuta la consulta y nos regresa un objeto del tipo indicad
@@ -41,37 +79,74 @@ public class EmpleadoDAOImpl implements EmpleadoDAO{
                 new EmpleadoRowMapper());
     }
 
-    //Eliminar todos los registros de la tabla
+    /**
+     * El método ejecuta un query para poder eliminar todos los registros de la
+     * base de datos
+     *
+     * @author Gerardo Martinez &lt;gerardo.martinez@dish.com.mx&gt;
+     * @return Regresa el número de filas eliminadas.
+     *
+     * @since 0.0.1
+     */
     @Override
     public int eliminarTodos() {
         return getJdbcTemplate().update("DELETE from empleado");
     }
 
-    //Eliminar registros de la tabla deacuerdo al ID
+    /**
+     * El método ejecuta un query para poder obtener todos los registros de la
+     * base de datos de acuerdo al id introducido.
+     *
+     * @author Gerardo Martinez &lt;gerardo.martinez@dish.com.mx&gt;
+     * @param id
+     * @return Regresa el número de filas eliminadas.
+     *
+     * @since 0.0.1
+     */
     @Override
     public int eliminarPorID(int id) {
         return getJdbcTemplate().update("DELETE FROM empleado WHERE num_empleado = ?", id);
     }
 
-    //Insertar empleados a la tabla
+    /**
+     * El método ejecuta un query para poder insertar registros en la base de
+     * datos de acuerdo a los datos que se le pasan en los argumentos del mismo.
+     *
+     * @author Gerardo Martinez &lt;gerardo.martinez@dish.com.mx&gt;
+     * 
+     * @param num_empleado
+     * @param nombre
+     * @param antiguedad
+     * @param fecha
+     * @param activo
+     *
+     * @since 0.0.1
+     */
     @Override
     public void insertarEmpleado(int num_empleado, String nombre, int antiguedad, String fecha, String activo) {
         getJdbcTemplate().update("INSERT INTO empleado (num_empleado, nombre, antiguedad, fecha_actualizacion, activo) "
                 + "VALUES(?,?,?,?,?)", num_empleado, nombre, antiguedad, fecha, activo);
     }
-    
-    
 
-    //Actualizar datos a la tabla
+    /**
+     * El método ejecuta un query para poder actualizar todos los registros de
+     * la base de datos de acuerdo a los datos que el usuario proporcione
+     *
+     * @author Gerardo Martinez &lt;gerardo.martinez@dish.com.mx&gt;
+     * @param empleado
+     * @return Regresa el número de filas actualizadas.
+     *
+     * @since 0.0.1
+     */
     @Override
     public int actualizarEmpleado(EmpleadoDTO empleado) {
         int resp = 0;
-        
+
         //Un update me regresa el número de filas actualizadas, es por eso
         //que el resultado es asignado a una variable de tipo int.
         return resp = getJdbcTemplate().update("UPDATE empleado SET nombre = ?, "
                 + "antiguedad = ?, fecha_actualizacion = ?, activo = ? "
-                + "WHERE  num_empleado= ?", new Object[]{empleado.getNombre(),empleado.getAntiguedad(), 
+                + "WHERE  num_empleado= ?", new Object[]{empleado.getNombre(), empleado.getAntiguedad(),
                     empleado.getFechaActualizacion(), empleado.getActivo(), empleado.getNumEmpleado()});
     }
 
@@ -95,5 +170,5 @@ public class EmpleadoDAOImpl implements EmpleadoDAO{
                 + "VALUES(?,?,?,?,?)", empleado.getNumEmpleado(), empleado.getNombre(), empleado.getAntiguedad(),
                 empleado.getFechaActualizacion(), empleado.getActivo());
     }
-   
+
 }
