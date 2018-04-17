e2a2e0a14ced87a90e231b009f153ca837fc65ad
Gerardo Martinez <gerardo.martinez@dish.com.mx>
2018-04-17 12:58:54 -0500

    Clase AppConfig documentada

--- src/main/java/com/dish/mx/dev/config/AppConfig.java
+++ src/main/java/com/dish/mx/dev/config/AppConfig.java
@@ -18,15 +18,29 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 @PropertySource("classpath:db.properties")
 @ComponentScan("com.dish.mx.dev")
 
+/**
+ * Clase en la que definimos los beans que vamos a cargar al contexto de spring.
+ *
+ * @version 0.0.1
+ *
+ * @author Gerardo Martinez &lt;gerardo.martinez@dish.com.mx&gt;
+ *
+ * @since 0.0.1
+ *
+ */
 public class AppConfig {
-    
+
     /**
-     * 
-     * @param env
-     * @return 
+     * El método recoge los datos necesarios para poder realizar la conexión a
+     * la base de datos cuando se necesite.
+     *
+     * @return Regresa un objeto con los certificados para la conexión.
+     *
+     * @author Gerardo Martinez &lt;gerardo.martinez@dish.com.mx&gt;
+     *
+     * @since 0.0.1
      */
     @Bean//(destroyMethod = "close")
-    //Interface para realizar la conexión a la base de datos.
     DataSource dataSource(Environment env) {
         //Creamos el objeto para poder utilizar sus métodos y conectarnos a la base de datos
         //ComboPooledDataSource ds = new ComboPooledDataSource();
@@ -42,35 +56,62 @@ public class AppConfig {
         ds.setUrl(env.getRequiredProperty("jdbc.url"));
         ds.setUsername(env.getRequiredProperty("jdbc.username"));
         ds.setPassword(env.getRequiredProperty("jdbc.password"));
-        
+
         return ds;
     }
 
-    //Creo mis Bean para poder llamarlos desdde mi MainApp
-    //Estos se cargarán al contexto de Spring para cuando requiera utilizarlos
+    /**
+     * Método para poder utilizar la conexión de la base con jdbcTemplate.
+     *
+     * @param dataSource
+     * @return Regresa la instancia jdbcTemplate con los datos obtenidos por el
+     * DataSource.
+     *
+     * @author Gerardo Martinez &lt;gerardo.martinez@dish.com.mx&gt;
+     *
+     * @since 0.0.1
+     */
     @Bean
     JdbcTemplate jdbcTemplate(DataSource dataSource) {
         return new JdbcTemplate(dataSource);
     }
-    
+
 //    @Bean
 //    JdbcTemplate jdbcTemplate2(DataSource dataSource) {
 //        return new JdbcTemplate(dataSource);
 //    }
-
+    /**
+     * Método para poder tener el acceso a los datos de la base
+     *
+     * @author Gerardo Martinez &lt;gerardo.martinez@dish.com.mx&gt;
+     *
+     * @since 0.0.1
+     */
     @Bean
     DataSourceTransactionManager dataSourceTransactionManager(DataSource dataSource) {
         return new DataSourceTransactionManager(dataSource);
     }
-    
+
+    /**
+     * Método para poder utilizar la variable jdbcTemplate en mi clase
+     * EmpleadoDAO y realizar las acividades necesarias con dicha variable.
+     *
+     * @param jdbcTemplate
+     * @return Regresa una instancia de EmpleadoDAO pasándole como argumento la
+     * variable jdbcTemplate para poder interactuar con la base de datos.
+     *
+     * @author Gerardo Martinez &lt;gerardo.martinez@dish.com.mx&gt;
+     *
+     * @since 0.0.1
+     */
     @Bean("empleadoDAOImpl2")
-    EmpleadoDAO empleadoDAOImpl(JdbcTemplate jdbcTemplate){
+    EmpleadoDAO empleadoDAOImpl(JdbcTemplate jdbcTemplate) {
         EmpleadoDAOImpl dao = new EmpleadoDAOImpl();
         dao.setJdbcTemplate(jdbcTemplate);
         return dao;
 //        return new EmpleadoDAOImpl(jdbcTemplate);
     }
-    
+
 //    @Bean
 //    CasosMenu menuEmpDao(EmpleadoDAOImpl empDAO){
 //        CasosMenu cm = new CasosMenu();
